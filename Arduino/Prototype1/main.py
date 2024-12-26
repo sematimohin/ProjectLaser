30faf09b97669f4cd4db908bca8e1a03c0c445d4
sematimohin <sematimohin@gmail.com>
2024-12-26 14:00:10 +0300

    design

---
+++ Arduino/Prototype1/main.py
@@ -0,0 +1,20 @@
+from PyQt5 import QtWidgets, uic
+from PyQt5.QtSerialPort import QSerialPort, QSerialPortInfo
+from PyQt5.QtCore import QIODevice
+
+app = QtWidgets.QApplication([])
+ui = uic.loadUi("design.ui")
+ui.setWindowTitle("SerialGUI")
+
+serial = QSerialPort()
+serial.setBaudRate(115200)
+portList = []
+ports = QSerialPortInfo().availablePorts()
+for port in ports:
+    portList.append(port.portName())
+ui.ComList.addItems(portList)
+
+
+
+ui.show()
+app.exec()
